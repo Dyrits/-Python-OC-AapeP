f0b2607bafb8cc7360f27407b13926bace63ffd8
Dyrits <61299170+Dyrits@users.noreply.github.com>
2020-03-11 23:04:54 +0100

    Add files via upload

---
+++ Pendu/data/donnees.py
@@ -0,0 +1,5 @@
+# Les mots rapportent un nombre de points différents, selon la difficulté de ceux-ci :
+dictionnaire_mots = {"SAUTERELLE" : 15, "CHIEN" : 15, "LAMPADAIRE" : 15, "LUNETTE" : 15, "MESSAGE" : 15, "CONSPIRATION" : 20, "PANIQUE" : 20, "WAGON" : 20, "APPRENTISSAGE" : 25, "RHINOCEROS" : 15, "CLAVIER" : 10, "TELEVISION" : 15, "OPERATION" : 15}
+
+# Le dictionnaire des joueurs et scores sera remplie lorsque le nombre de joueurs sera déterminés et leurs noms saisis.
+dictionnaire_scores = {}
