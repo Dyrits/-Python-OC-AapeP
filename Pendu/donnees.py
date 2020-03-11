e974d2278c0e634799dfd82d4a7e3f55e4a22d59
Dyrits <61299170+Dyrits@users.noreply.github.com>
2020-03-11 23:04:12 +0100

    Delete donnees.py

--- Pendu/donnees.py
+++
@@ -1,5 +0,0 @@
-# Les mots rapportent un nombre de points différents, selon la difficulté de ceux-ci :
-dictionnaire_mots = {"SAUTERELLE" : 15, "CHIEN" : 20, "LAMPADAIRE" : 15, "LUNETTE" : 15, "MESSAGE" : 15, "CONSPIRATION" : 30, "PANIQUE" : 20, "WAGON" : 20, "APPRENTISSAGE" : 25}
-
-# Le dictionnaire des joueurs et scores sera remplie lorsque le nombre de joueurs sera déterminés et leurs noms saisis.
-dictionnaire_scores = {}
